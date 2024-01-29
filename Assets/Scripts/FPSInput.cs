a407616da9713dd1a263d6b08d9193f82319dcff
kcaparas1630 <ICS21-137@camosun.bc.ca>
2024-01-29 14:07:22 -0800

    Finished Lab4
    Finished Lab 4 added Couroutines and updated CharacterController component

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
+    private float gravity = -9.8f;
     private float speed = 9.0f;
-    float horizInput;
-    float vertInput;
+    [SerializeField]
+    private CharacterController characterController;
+    private float horizInput;
+    private float vertInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,20 @@ public class FPSInput : MonoBehaviour
     {
         horizInput = Input.GetAxis("Horizontal");
         vertInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizInput, 0, vertInput) * speed * Time.deltaTime;
-        transform.Translate(movement);
+        Vector3 movement = new Vector3(horizInput, 0, vertInput);
+
+        //Clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+        //apply gravity to player
+        movement.y = gravity;
+        //take speed into account
+        movement *= speed;
+        //make movement processor independent
+        movement *= Time.deltaTime;
+
+        //Convert local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        characterController.Move(movement);
     }
 }

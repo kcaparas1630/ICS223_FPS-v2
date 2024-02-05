7d0ba0b9dd866c842f8036b1ee0332f6e5a6242e
kcaparas1630 <ICS21-137@camosun.bc.ca>
2024-02-05 13:14:52 -0800

    Update RayShooter.cs
    Updated GetMouseButton to GetMouseButtonDown to avoid spamming bullets

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -18,7 +18,7 @@ public class RayShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)) {
+        if(Input.GetMouseButtonDown(0)) {
             Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
             Ray ray = cam.ScreenPointToRay(point);
             RaycastHit hit;
